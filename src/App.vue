b1d779dc5ffc81da9d78c2deef9200a608abc37f
ian.davis <ian.davis@salemwebnetwork.com>
2022-05-02 16:33:50 -0400

    .

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@ import JokeOptionsApi from '@/components/JokeOptionsApi.vue'
 </script>
 
 <template>
-    <img class="lol" src="./assets/lol.svg" />
-    <JokeOptionsApi />
+    <div class="img"><img class="lol" src="./assets/lol.svg" /></div>
+    <div class="cols">
+        <JokeOptionsApi />
+        <JokeOptionsApi />
+    </div>
     <!--<JokeCompositionApi /> -->
 </template>
 
 <style>
 @import './assets/base.css';
+.cols { display:grid; grid-template-columns: 300px 300px; }
+.cols >div:first-child { border-right: 2px solid #999; padding-right: 40px; }
+.cols >div:last-child { padding-left: 40px; }
 </style>
--- src/components/JokeOptionsApi.vue
+++ src/components/JokeOptionsApi.vue
@@ -1,26 +1,36 @@
 <script>
-import jokes from '@/assets/jokes.json'
+import jokes from "@/assets/jokes.json";
 
 export default {
     data() {
         return {
             jokeIdx: 0,
-            joke: ``
-        }
+            joke: ``,
+        };
     },
     methods: {
-        next() {
+        refresh() {
             this.jokeIdx++
+            if (this.jokeIdx == jokes.length) { // start over
+                this.jokeIdx = 0
+            }
+            this.joke = jokes[this.jokeIdx]
         },
     },
+    computed: {
+        jokeWithLineBreaks() {
+            return this.joke.replaceAll(`\n`,`<br/>`)
+        }
+    },
     mounted() {
-        this.joke = jokes[this.jokeIdx]
-    },    
+        this.joke = jokes[this.jokeIdx];
+    },
 };
 </script>
 
 <template>
-    {{joke}}
+    <div>
+        <div v-html="jokeWithLineBreaks"></div>
+        <button @click.prevent="refresh">Refresh</button>
+    </div>
 </template>
-
-<style scoped></style>
