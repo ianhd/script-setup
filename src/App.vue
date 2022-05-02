7df5c5a1f61748b3156bdccf4be8ec4e2e9432c1
ian.davis <ian.davis@salemwebnetwork.com>
2022-05-02 16:08:28 -0400

    .

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import JokeOptionsApi from '@/components/JokeOptionsApi.vue'
+// import JokeCompositionApi from '@/components/JokeCompositionApi.vue'
+</script>
+
+<template>
+    <img class="lol" src="./assets/lol.svg" />
+    <JokeOptionsApi />
+    <!--<JokeCompositionApi /> -->
+</template>
+
+<style>
+@import './assets/base.css';
+</style>
---
+++ src/components/JokeOptionsApi.vue
@@ -0,0 +1,26 @@
+<script>
+import jokes from '@/assets/jokes.json'
+
+export default {
+    data() {
+        return {
+            jokeIdx: 0,
+            joke: ``
+        }
+    },
+    methods: {
+        next() {
+            this.jokeIdx++
+        },
+    },
+    mounted() {
+        this.joke = jokes[this.jokeIdx]
+    },    
+};
+</script>
+
+<template>
+    {{joke}}
+</template>
+
+<style scoped></style>
